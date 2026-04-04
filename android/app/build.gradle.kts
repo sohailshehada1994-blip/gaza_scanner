03343e02118b6f59976dbc2ed036ab903b07bb9c
sohailshehada1994-blip <sohailshehada1994@gmail.com>
2026-04-04 19:28:21 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -46,4 +47,3 @@ flutter {
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
 }
-apply plugin: 'com.google.gms.google-services'
