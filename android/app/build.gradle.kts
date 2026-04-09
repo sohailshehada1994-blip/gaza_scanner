0a9057eccb6dc5101a5ec573b429de6af125db1c
sohailshehada1994-blip <sohailshehada1994@gmail.com>
2026-04-09 18:27:00 +0300

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     }
 
     signingConfigs {
-        create("debug") {
+        getByName("debug") {
             storeFile = file("debug.keystore")
             storePassword = "android"
             keyAlias = "androiddebugkey"
