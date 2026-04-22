f71d8309ccb01845fbb86c9bd7679e4077136a76
sohailshehada1994-blip <sohailshehada1994@gmail.com>
2026-04-22 17:50:54 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // محرك الأندرويد الأساسي
+        classpath("com.android.tools.build:gradle:8.2.1")
+        // المحرك المسؤول عن قراءة ملف google-services.json
+        classpath("com.google.gms:google-services:4.4.1")
+        // دعم لغة كوتلن
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.22")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -5,6 +20,7 @@ allprojects {
     }
 }
 
+// كود تنظيم مسارات البناء (الذي تستخدمه في مشروعك)
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
@@ -15,13 +31,11 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-plugins {
-    id("com.google.gms.google-services") version "4.4.4" apply false
 }
